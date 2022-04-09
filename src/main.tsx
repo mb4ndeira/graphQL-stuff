bcb9022d8a73cdc2e694317b3b059baed8199e52
mb4ndeira <mbandeira.cs@gmail.com>
2022-04-09 13:08:15 +0000

    feat: implement graphQL with Apollo on the application

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+import { ApolloProvider } from '@apollo/client'
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import { client } from './lib/apollo'
+
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
