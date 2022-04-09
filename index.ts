a0c7a0180ef19558254769f3aadcdc78470e0760
mb4ndeira <mbandeira.cs@gmail.com>
2022-04-09 10:48:02 +0000

    Initialize repository

---
+++ index.ts
@@ -0,0 +1,20 @@
+import path from 'path'
+import { ApolloServer } from 'apollo-server'
+import { buildSchema } from 'type-graphql'
+
+async function main() {
+    const schema = await buildSchema({
+        resolvers: [],
+        emitSchemaFile: path.resolve(__dirname, 'schema.gql')
+    })
+
+    const server = new ApolloServer({
+        schema,
+    })
+
+    const { url } = await server.listen()
+
+    console.log(url)
+}
+
+main()
